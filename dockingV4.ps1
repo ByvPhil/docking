4c392b72cdd9ac49f8ac3ebc0f32ecabc2d3a4c1
ByvPhil <163297886+ByvPhil@users.noreply.github.com>
2024-04-23 16:33:01 +0200

    Update dockingV4.ps1

--- dockingV4.ps1
+++ dockingV4.ps1
@@ -1,2 +1,9 @@
-Get-WmiObject -Class Win32_PnPEntity | Where-Object {($_.name -like "*Displaylink*" -or $_.name -like "*dock*" -or $_.Caption -like "*docking*" )-and $_.Status -like "OK"}  | Select-Object Name, Manufacturer, DeviceID
+$devices = Get-WmiObject -Class Win32_PnPEntity | Where-Object {($_.Name -like "*Displaylink*" -or $_.Name -like "*dock*" -or $_.Caption -like "*docking*") -and $_.Status -like "OK"} | Select-Object Name, Manufacturer, DeviceID
+
+foreach ($device in $devices) {
+    "Nom : $($device.Name)"
+    "Fabricant : $($device.Manufacturer)"
+    "ID du périphérique : $($device.DeviceID)"
+    "--------------------------"
+}
 exit
